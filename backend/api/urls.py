8f8909df27f72c9cf119519593fdcdf585bc9fa3
helloGithub326 <pranay.aluri@gmail.com>
2025-05-19 18:49:53 -0500

    Fixed api.urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('skinImage/', views.skinImage, name="Skin Image"),
-    path('eyeImage/', views.eyeImage, name="Eye Image")
+    path('skinImage/', views.skinImage, name="Skin Image")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
